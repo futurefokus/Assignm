6b64b4b32ded47ffc8cebd04d068bc73b030d5e2
futurefokus <futurefokus24@gmail.com>
2024-12-26 20:11:03 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import InventoryManagement.Product;
+import InventoryManagement.Supplier;
+import InventoryManagement.Inventory;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product[] products = new Product[3];
+        products[0] = new Product("Laptop", 101, 1200.00);
+        products[1] = new Product("Smartphone", 102, 800.00);
+        products[2] = new Product("Tablet", 103, 600.00);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Inventory[] inventories = new Inventory[3];
+        inventories[0] = new Inventory("Pinduoduo", "Kazakhstan", products);
+        inventories[1] = new Inventory("AliExpress", "China", products);
+        inventories[2] = new Inventory("Ozon", "Russia", products);
+        inventories[0].displayProducts();
+        System.out.println();
+
+        Supplier supplier1 = new Supplier("TechSupplier1", 201, products[1]);
+        Supplier supplier2 = new Supplier("TechSupplier2", 202, products[0]);
+        Supplier supplier3 = new Supplier("TechSupplier3", 203, products[2]);
+
+        System.out.println(supplier1.getProductDetails());
+        System.out.println(supplier2.getProductDetails());
+        System.out.println(supplier3.getProductDetails());
+        System.out.println();
+
+        supplier1.isOlderThan(supplier2);
+        supplier2.isOlderThan(supplier3);
     }
 }
